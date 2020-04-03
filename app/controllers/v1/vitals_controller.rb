9421ca689b1b66639d3ea0a16b6f738cdb376b35
adriaanbd <adriaanbd@gmail.com>
2020-04-03 00:11:42 -0500

    query for vitals controller

--- app/controllers/v1/vitals_controller.rb
+++ app/controllers/v1/vitals_controller.rb
@@ -5,14 +5,23 @@ class V1::VitalsController < ApplicationController
     vitals = find_vitals_by_user
     return unless vitals
 
-    today = vitals.today
-    yesterday = vitals.yesterday
-    last_week = vitals.last_week
-    render json: { today: today, yesterday: yesterday, last_week: last_week }, status: 200
+    query = params[:day]
+    if query === 'today'
+      data = vitals.today
+    elsif query === 'yesterday'
+      data = vitals.yesterday
+    elsif query === 'last_week'
+      data = vitals.last_week
+    else
+      data = { vitals: {today: vitals.today, yesterday: vitals.yesterday, last_week: vitals.last_week} }
+    end
+    # byebug
+    render json: data, status: 200
   end
 
   def create
-    vital = Vital.new(vital_params)
+    user = User.find(params[:user_id])
+    vital = user.vitals.build(vital_params)
     if vital.save
       render json: vital, status: 201
     else
@@ -23,7 +32,7 @@ class V1::VitalsController < ApplicationController
   private
 
   def vital_params
-    params.require(:vital).permit(:category, :measure, :user_id)
+    params.require(:vital).permit(:category, :measure)
   end
 
   def find_vitals
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :vitals, dependent: :destroy
 end
