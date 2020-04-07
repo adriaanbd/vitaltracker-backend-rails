cb9ed14135c7394343859c698a8e5b1642534f8d
adriaanbd <adriaanbd@gmail.com>
2020-04-07 09:50:26 -0500

    adding dotenv and env variable to suppress ruby 2.7 deprecation warnings

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ group :development, :test do
   gem 'pry-rails'
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+  gem 'dotenv-rails'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+HOSTNAME = ENV['RUBY_OPT']
+
 module VitaltrackerBackendRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
