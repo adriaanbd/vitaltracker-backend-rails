e5591eda48d3b50b9d5e9519cf9dcdaf045dcdeb
adriaanbd <adriaanbd@gmail.com>
2020-04-07 09:56:12 -0500

    done implementing validations specs on vitals

--- spec/models/vital_spec.rb
+++ spec/models/vital_spec.rb
@@ -19,5 +19,34 @@ RSpec.describe Vital, type: :model do
       valid_vital.valid?
       expect(valid_vital.errors[:measure]).to include("can't be blank");
     end
+
+    it 'is valid with a weight attribute' do
+      valid_vital.valid?
+      expect(valid_vital.errors[:category]).to_not include("can't be blank");
+    end
+
+    it 'is not valid without a weight attribute' do
+      valid_vital.category = nil
+      valid_vital.valid?
+      expect(valid_vital.errors[:category]).to include("can't be blank");
+    end
+
+    it 'is valid with a valid category attribute' do
+      valid_vital.valid?
+      expect(valid_vital.errors[:category]).to_not include("can't be blank");
+    end
+
+    it 'is not valid without a category attribute' do
+      valid_vital.category = nil
+      valid_vital.valid?
+      expect(valid_vital.errors[:category]).to include("can't be blank");
+    end
+
+    it 'is not valid without a valid category attribute' do
+      invalid_category = "invalid category"
+      valid_vital.category = invalid_category
+      valid_vital.valid?
+      expect(valid_vital.errors[:category]).to include("invalid category is not a valid vital category");
+    end
   end
 end
